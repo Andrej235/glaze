4f831851acbd1a9c2d8755e90aa486687f4a69b4
Andrej235 <andrej.nenadic06@gmail.com>
2025-10-12 16:36:01 +0200

    Create vector2, vector3, and transform component

---
+++ engine/src/components/transform.zig
@@ -0,0 +1,35 @@
+const Vector2 = @import("../utils/vector2.zig").Vector2;
+const Vector3 = @import("../utils/vector3.zig").Vector3;
+const GameObject = @import("../scene-manager/game_object.zig").GameObject;
+
+pub const Transform = struct {
+    game_object: ?*GameObject = null,
+
+    position: Vector3 = Vector3.init(0.0, 0.0, 0.0),
+    rotation: Vector3 = Vector3.init(0.0, 0.0, 0.0),
+    scale: Vector3 = Vector3.init(1.0, 1.0, 1.0),
+
+    pub fn create(ptr: *Transform) !void {
+        ptr.* = Transform{
+            .game_object = undefined,
+        };
+    }
+
+    pub fn setPosition(self: *Transform, position: Vector3) void {
+        self.position.x = position.x;
+        self.position.y = position.y;
+        self.position.z = position.z;
+    }
+
+    pub fn setRotation(self: *Transform, rotation: Vector3) void {
+        self.rotation.x = rotation.x;
+        self.rotation.y = rotation.y;
+        self.rotation.z = rotation.z;
+    }
+
+    pub fn setScale(self: *Transform, scale: Vector3) void {
+        self.scale.x = scale.x;
+        self.scale.y = scale.y;
+        self.scale.z = scale.z;
+    }
+};
--- engine/src/root.zig
+++ engine/src/root.zig
@@ -4,10 +4,18 @@ const Debug = @import("debug/debug.zig").Debug;
 const WL = @import("platform/linux/wayland.zig").Wayland;
 const Renderer = @import("renderer/renderer.zig").Renderer;
 
+const Transform = @import("components/transform.zig").Transform;
+
 const App = @import("app.zig").App;
 
 pub fn main() !void {
-    _ = try App.create();
+    const app = try App.create();
+
+    //#region test scene
+    const scene = try app.scene_manager.createScene("scene-1");
+    const go = try scene.addEntity();
+    _ = try go.addComponent(Transform);
+    //#endregion
 
     while (true) {
         Debug.logFps();
